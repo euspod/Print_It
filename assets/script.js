055b9ff08adf22112b01c1ae925a8a47bff4c57f
euspod <euspod@hotmail.fr>
2023-05-22 13:14:29 +0400

    Ajout code javascript

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,59 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const bannerImg = document.querySelector(".banner-img");
+const dots = document.querySelectorAll(".dot");
+var slidePosition = 0;
+
+function SetActive () {
+	
+	dots.forEach(dot => {
+		dot.classList.remove('dot_selected');
+		}
+	);
+	dots[slidePosition].classList.add('dot_selected');
+	console.log(dots);
+}
+
+
+function SetImage () {
+	bannerImg.src = "./assets/images/slideshow/" + slides[slidePosition].image;
+	document.querySelector("#banner > p").innerHTML = slides[slidePosition].tagLine;
+}
+
+
+function SetNext () {
+	
+	if (slidePosition < slides.length -1 )
+		{
+			slidePosition ++ ;
+		} 
+		else {
+			slidePosition = 0;
+		}
+
+	SetImage();
+	SetActive();
+}
+
+function SetPrev () {
+	
+	if (slidePosition === 0 )
+		{
+			slidePosition = slides.length -1;
+		} 
+		else {
+			slidePosition --;
+		}
+
+	SetImage();
+	SetActive();
+}
+
+arrowLeft.addEventListener("click",  SetPrev);
+arrowRight.addEventListener("click",  SetNext);
+
